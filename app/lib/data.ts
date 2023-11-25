24d2bb4a5b953f324fe5c985d4519a088bbff964
Jonathan Rovner <jonrovner@gmail.com>
2023-11-25 08:21:01 -0300

    completed up tu chapter 14

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -174,10 +174,12 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
+    console.log(invoice);
 
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
+    throw new Error('Failed to fectch invoice');
   }
 }
 
